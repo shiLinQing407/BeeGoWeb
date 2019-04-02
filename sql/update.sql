1155f08eb843c5c340a431b4435111d7897c22ec
KWJ <1652496339@qq.com>
2019-04-03 00:12:31 +0800

    tab选项卡的添加、关闭

--- sql/update.sql
+++ sql/update.sql
@@ -7,7 +7,7 @@ CREATE TABLE `go_news` (
   `content` text NOT NULL,
   `create_time` int(11) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资讯表'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资讯表';
 
 CREATE TABLE `go_news_class` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -15,7 +15,7 @@ CREATE TABLE `go_news_class` (
   `class_name_en` varchar(30) DEFAULT NULL COMMENT '分类名称 英文',
   `create_time` int(11) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资讯分类表'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资讯分类表';
 
 insert into `go_web`.`go_menu` ( `parent_id`, `title_cn`, `title_en`, `icon`, `url_for`, `type`, `logic_delete`, `update_time`, `create_time`, `sort`) values ( '0', '资讯管理', null, '', null, '1', '0', null, null, '0');
 insert into `go_web`.`go_menu` ( `parent_id`, `title_cn`, `title_en`, `icon`, `url_for`, `type`, `logic_delete`, `update_time`, `create_time`, `sort`) values ( '400', '资讯列表', null, '', 'NewsController.Index', '1', '0', null, null, '0');
