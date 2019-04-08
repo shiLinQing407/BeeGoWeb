7045330df67a2defd8ffe62f3eeadb0af09c9443
KWJ <1652496339@qq.com>
2019-04-08 16:06:44 +0800

    资讯新增功能修改

--- models/news/News.go
+++ models/news/News.go
@@ -13,7 +13,7 @@ import (
 //资讯
 type News struct {
 	Id          	int		`json:"id" orm:"auto"`
-	Class_id		int		`json:"class_id"`
+	ClassId		int		`json:"class_id"`
 	Tags			string	`json:"tags"`
 	Title			string	`json:"title"`
 	Content			string	`json:"content"`
